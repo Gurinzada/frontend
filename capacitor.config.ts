319b7f46f6fc7f58fa9c6958b55140d402f2a48b
Augusto <augustoinacio243@gmail.com>
2026-03-22 22:07:55 -0300

    fix: update webDir in capacitor.config.ts from 'build' to 'dist'

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.ufla.repohound',
   appName: 'RepoHound',
-  webDir: 'build'
+  webDir: 'dist'
 };
 
 export default config;
